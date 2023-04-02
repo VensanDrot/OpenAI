e259f061928c1228ed8a83a2bd882252514a9f3f
Vensan <v.drot@student.inha.uz>
2023-04-03 01:50:50 +0500

    package change

--- gptback/index.js
+++ gptback/index.js
@@ -11,13 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  })
-);
+app.use(cors());
 
 const configuration = new Configuration({
   organization: "org-tZ4PHQU8zeNFFEG6WV6JpvH2",
